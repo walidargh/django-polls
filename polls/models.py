90163cd5b4112d446a7113dfaa3148d809db8fa8
Walid Arghandewal <walid.argh@gmail.com>
2016-06-11 00:29:25 -0700

    added items to database

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
@@ -9,7 +12,7 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= timezone.noew - datetime.timedelta(days=1)
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)
