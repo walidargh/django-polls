2f0c940db51f3dfde8802ec9c3378112183d1252
Walid Arghandewal <walid.argh@gmail.com>
2016-06-12 13:45:49 -0700

    functioning index page

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join(q.question_text for q in latest_question_list_question_list)
-	return HttpResponse(output)
+	# output = ', '.join(q.question_text for q in latest_question_list_question_list)
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at the question %s." % question_id)
