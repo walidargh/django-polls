d9e095e0ed0d6e2e19e7cbc0409e7c6a47b7e49d
Walid Arghandewal <walid.argh@gmail.com>
2016-06-12 13:37:24 -0700

    index displays 5 most recent

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-		return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join(q.question_text for q in latest_question_list_question_list)
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at the question %s." % question_id)
@@ -15,6 +17,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
-	
+
 
 # Create your views here.
